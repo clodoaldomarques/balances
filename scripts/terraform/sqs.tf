2341e6817ee2c4942f43fd5d14761d9ee0b76a14
clodoaldomarques <clodoaldomarques@gmail.com>
2026-05-16 21:46:53 -0300

    removendo referencias

--- scripts/terraform/sqs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sqs_queue" "balances-sqs-queue" {
-  name = "balances-sqs-queue"
-}
-
-resource "aws_sns_topic_subscription" "balances-sns-sqs-subscription" {
-  topic_arn = aws_sns_topic.balances-sns-topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.balances-sqs-queue.arn 
-}
